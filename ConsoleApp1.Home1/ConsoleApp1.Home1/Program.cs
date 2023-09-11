f3ecf0e2f6656bb43b3cb4d87ca1354143c165b2
Isaev-El <isaev.elkhan2003@gmail.com>
2023-09-11 20:47:12 +0600

    homework number 1

--- ConsoleApp1.Home1/ConsoleApp1.Home1/Program.cs
+++ ConsoleApp1.Home1/ConsoleApp1.Home1/Program.cs
@@ -8,11 +8,83 @@ namespace ConsoleApp1.Home1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("sdvadvas");
-            Console.ReadLine();
+            printSumOfNumbers();
+        }
+        
+        static void printFIO()
+        {
+            string name, surname, lastname;
+
+            bool count = true;
+
+            while (count) {
+
+                Console.Write("Введите имя: ");
+                name = Console.ReadLine();
+
+                Console.Write("Введите фамилию: ");
+                surname = Console.ReadLine();
+
+                Console.Write("Введите отчество: ");
+                lastname = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(name) || 
+                    string.IsNullOrWhiteSpace(surname) || 
+                    string.IsNullOrWhiteSpace(lastname) || 
+                    int.TryParse(name, out _) ||
+                    int.TryParse(surname, out _) ||
+                    int.TryParse(lastname, out _))
+                {
+                    Console.WriteLine("Ошибка! Вы ввели значение " +
+                        "отличающееся от String или значение равно null! " +
+                        "Нажмите Enter и введите данные еще раз");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    count = false;
+                    Console.WriteLine("Данные успешно введены!");
+                    Console.WriteLine("Имя: " + name);
+                    Console.WriteLine("Фамилия: " + surname);
+                    Console.WriteLine("Отчество: " + lastname);
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        static void printSumOfNumbers()
+        {
+            bool count = true;
+
+            while (count)
+            {
+                Console.Write("Введите первое число:");
+                string First = Console.ReadLine();
+
+                Console.Write("Введите второе число:");
+                string Second = Console.ReadLine();
+
+                Console.Write("Введите третье число:");
+                string Thirt = Console.ReadLine();
+
+                if (double.TryParse(First, out double first) &&
+                    double.TryParse(Second, out double second) &&
+                    double.TryParse(Thirt, out double thirt))
+                {
+                    count = false;
+                    double Sum = first + second + thirt;
+                    Console.Write("Сумма чисел:" + Sum);
+                    Console.ReadLine();
+                }
+                else
+                {          
+                    Console.Write("Введите число (int or double)! Нажмите Enter, чтобы продолжить!");
+                    Console.ReadLine();
+                }
+            }
         }
     }
+
 }
