1a0156a40757e13d44c3d6e615bcfa03ea019455
Isaev-El <isaev.elkhan2003@gmail.com>
2023-09-11 23:52:03 +0600

    Practic two

--- ConsoleApp.Lesson1/ConsoleApp.Lesson1/Program.cs
+++ ConsoleApp.Lesson1/ConsoleApp.Lesson1/Program.cs
@@ -10,11 +10,293 @@ namespace ConsoleApp1.Lesson1
     {
         static void Main(string[] args)
         {
-            example8();
+            taskTen();
             Console.ReadLine();
+            
         }
+        static (int, int) taskTwenty(int a, int b)
+        {
+            return (b, a);
 
-        static void example8()
+        }
+        static void taskTwentyOne(ref int a, ref int b)
+        {
+            (b, a) = (a, b);
+        }
+        static double taskTwentyTwo(int x, int y)
+        {
+            if (x == 0 && y == 0)
+            {
+                return 0;
+            }
+            else if (y == 0)
+            {
+                return 12.0 / x;
+            }
+            else if (x == 0)
+            {
+                return 12.0 / y;
+            }
+            else
+            {
+                return 144.0 / (x * y);
+            }
+        }
+        static int taskTwentyThree(int h, int m, int s)
+        {
+            if (h < 0 || h >= 24 || m < 0 || m >= 60 || s < 0 || s >= 60)
+            {
+                return -1;
+            }
+
+            int totalSeconds = h * 3600 + m * 60 + s;
+
+            return totalSeconds;
+        }
+        static int taskTwentyFour(int m, int d)
+        {
+            if (m < 1 || m > 12 || d < 1 || d > 30)
+            {
+                return -1;
+            }
+
+            int totalDays = (m - 1) * 30 + d;
+
+            return totalDays;
+        }
+        static int taskTwentyFive(int m, int d)
+        {
+            int[] daysInMonth = { 31, 30, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+            if (m < 1 || m > 12 || d < 1 || d > daysInMonth[m - 1])
+            {
+                return -1;
+            }
+
+            int totalDays = 0;
+
+            for (int i = 0; i < m - 1; i++)
+            {
+                totalDays += daysInMonth[i];
+            }
+
+            totalDays += d;
+
+            return totalDays;
+        }
+        static int taskTwentySix(int m1 = 999, int m2 = 999, int m3 = 999)
+        {
+            if (m1 < 1 || m1 > 999) m1 = 999;
+            if (m2 < 1 || m2 > 999) m2 = 999;
+            if (m3 < 1 || m3 > 999) m3 = 999;
+
+            int minNumber = m1;
+
+            if (m2 < minNumber)
+            {
+                minNumber = m2;
+            }
+            if (m3 < minNumber)
+            {
+                minNumber = m3;
+            }
+
+            return minNumber;
+        }
+        static bool taskTwentySeven(int n)
+        {
+            return n % 2 == 0;
+        }
+        static int taskTwentyEight(int a, int b, int c)
+        {
+
+            if (a <= b && a <= c)
+            {
+                return a;
+            }
+            else if (b <= a && b <= c)
+            {
+                return b;
+            }
+            else
+            {
+                return c;
+            }
+
+        }
+        static void taskTwentyNine()
+        {
+            Random rand = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                int grade = rand.Next(2, 6);
+                string message;
+
+                switch (grade)
+                {
+                    case 2:
+                        message = "Неуд";
+                        break;
+                    case 3:
+                        message = "Удовл";
+                        break;
+                    case 4:
+                        message = "Хорошо";
+                        break;
+                    case 5:
+                        message = "Отлично";
+                        break;
+                    default:
+                        message = "Недопустимая оценка";
+                        break;
+                }
+
+                Console.WriteLine($"Оценка: {grade}, Результат: {message}");
+            }
+        }
+        static int taskThirty(int M, int N)
+        {
+            int sum = 0;
+
+            // Проверяем, чтобы M было нечетным, иначе увеличиваем M на 1
+            if (M % 2 == 0)
+            {
+                M++;
+            }
+
+            for (int i = M; i <= N; i += 2)
+            {
+                sum += i;
+            }
+
+            return sum;
+        }
+        static int taskThirtyOne(int N, int n)
+        {
+
+            int result = N;
+
+            while (result % n != 0)
+            {
+                result++;
+            }
+
+            return result;
+        }
+        static void taskTen()
+        {
+            Console.WriteLine("Выберите задание:");
+            Console.WriteLine("a) Все целые числа от 20 до 35.");
+            Console.WriteLine("b) Квадраты всех целых чисел от 10 до b (значение b вводится с клавиатуры; b > 10).");
+            Console.WriteLine("c) Третьи степени всех целых чисел от a до 50 (значение a вводится с клавиатуры; a < 50).");
+            Console.WriteLine("d) Все целые числа от a до b (значения a и b вводятся с клавиатуры; b > a).");
+
+            char choice;
+
+            while (!char.TryParse(Console.ReadLine(), out choice) || (choice != 'a' && choice != 'b' && choice != 'c' && choice != 'd'))
+            {
+                Console.WriteLine("Выбор задания некорректен. Попробуйте ещё раз:");
+            }
+
+            Console.WriteLine();
+
+            switch (choice)
+            {
+                case 'a':
+                    for (int i = 20; i <= 35; i++)
+                    {
+                        Console.WriteLine(i);
+                    }
+                    break;
+                case 'b':
+                    int b;
+                    do
+                    {
+                        if (TryParseInt("Введите значение b (b > 10):", out b) && b > 10)
+                        {
+                            break;
+                        }
+                        Console.WriteLine("Значение b должно быть больше 10. Попробуйте ещё раз:");
+                    } while (true);
+
+                    for (int i = 10; i <= b; i++)
+                    {
+                        Console.WriteLine(i * i);
+                    }
+                    break;
+                case 'c':
+                    int a;
+                    do
+                    {
+                        if (TryParseInt("Введите значение a (a < 50):", out a) && a < 50)
+                        {
+                            break;
+                        }
+                        Console.WriteLine("Значение a должно быть меньше 50. Попробуйте ещё раз:");
+                    } while (true);
+
+                    for (int i = a; i <= 50; i++)
+                    {
+                        Console.WriteLine(Math.Pow(i, 3));
+                    }
+                    break;
+                case 'd':
+                    int start, end;
+                    do
+                    {
+                        if (TryParseInt("Введите значение a:", out start) &&
+                            TryParseInt("Введите значение b:", out end) && end > start)
+                        {
+                            break;
+                        }
+                        Console.WriteLine("Значение b должно быть больше a. Попробуйте ещё раз:");
+                    } while (true);
+
+                    for (int i = start; i <= end; i++)
+                    {
+                        Console.WriteLine(i);
+                    }
+                    break;
+            }
+        }
+        static bool TryParseInt(string message, out int result)
+        {
+            Console.Write(message + " ");
+            return int.TryParse(Console.ReadLine(), out result);
+        }
+        static void taskNine()
+        {
+            Console.Write("Введите сопротивление для первого участка: ");
+            double resistance1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите напряжение для первого участка: ");
+            double voltage1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите сопротивление для второго участка: ");
+            double resistance2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите напряжение для второго участка: ");
+            double voltage2 = Convert.ToDouble(Console.ReadLine());
+
+            double current1 = voltage1 / resistance1;
+            double current2 = voltage2 / resistance2;
+
+            if (current1 < current2)
+            {
+                Console.WriteLine("Ток на участке 1 меньше: " + current1 + " Ампер");
+                Console.ReadLine();
+            }
+            else if (current2 < current1)
+            {
+                Console.WriteLine("Ток на участке 2 меньше: " + current2 + " Ампер");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Токи на обоих участках равны: " + current1 + " Ампер");
+                Console.ReadLine();
+            }
+        }
+        static void taskEight()
         {
             double massOfFirstObject = 0;
             double massOfSecondObject = 0;
@@ -43,7 +325,7 @@ namespace ConsoleApp1.Lesson1
                 Console.WriteLine("Equals");
             }
         }
-        static void example7()
+        static void taskSeven()
         {
             int squareSide = 0;
             int radius = 0;
@@ -65,7 +347,7 @@ namespace ConsoleApp1.Lesson1
                 Console.WriteLine("Equal");
             }
         }
-        static void example6()
+        static void taskSix()
         {
             bool a = true;
             bool b = false;
@@ -75,37 +357,63 @@ namespace ConsoleApp1.Lesson1
             bool f = b || c;
             Console.WriteLine(d + " " + e + " " + f);
         }
-        static void periodToweeks()
+        static void taskFive()
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.Write("Введите число:");
+                string a = Console.ReadLine();
+                if (int.TryParse(a, out int number))
+                {
+                    if (number / 10>0 && number/10<10) {
+                        flag = false;
+                        Console.Write("Число десятков:" + number / 10);
+                        Console.WriteLine();
+                        Console.Write("Число единиц:" + number % 10);
+                        Console.WriteLine();
+                        Console.Write("Сумма цифр:" + ((number / 10) + (number % 10)));
+                        Console.WriteLine();
+                        Console.Write("Произведение цифр:" + ((number / 10) * (number % 10)));
+                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Введите двузначное число!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка введите допустимый тип переменной! (int). Нажмите Enter");
+                    Console.ReadLine();
+                }
+            }
+        }
+        static void taskFour()
         {
             int days = 234;
-            Console.WriteLine(234 / 7);
+            Console.WriteLine(days / 7);
         }
-        static int santToMetr()
+        static int taskThree()
         {
             int santimetr = 0;
             santimetr = Convert.ToInt32(Console.ReadLine());
             return (santimetr / 100);
         }
-        static void printJust()
+        static void taskSecond()
+        {
+            Console.WriteLine(5);
+            Console.WriteLine(10);
+            Console.WriteLine(21);
+        }
+        static void taskFirst()
         {
             int a, b, c = 0;
             a = Convert.ToInt32(Console.ReadLine());
             b = Convert.ToInt32(Console.ReadLine());
             c = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(a + " " + b + " " + c + " ");
+            Console.WriteLine(a + "  " + b + "  " + c);
         }
-        static void converExample()
-        {
-            int a = Convert.ToInt32("5");
-
-        }
-
-        static void dateExample()
-        {
-            DateTime dt = new DateTime(2023, 01, 01);
-
-        }
-
         static int printSum()
         {
             int i, j = 0;
@@ -113,7 +421,6 @@ namespace ConsoleApp1.Lesson1
             j = Convert.ToInt32(Console.ReadLine());
             return i + j;
         }
-
         static void printName()
         {
             string name = "", surname = "";
