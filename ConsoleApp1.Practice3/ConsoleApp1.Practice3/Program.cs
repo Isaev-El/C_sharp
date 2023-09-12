8a7769e9da8f8923ec9f3480fca1f7cdbe3ebdd7
Isaev-El <isaev.elkhan2003@gmail.com>
2023-09-12 22:54:51 +0600

    pactice 3

---
+++ ConsoleApp1.Practice3/ConsoleApp1.Practice3/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Practice3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StrF11();
+        }
+
+        static void F10()
+        {
+            char a = '0';
+
+            while (a != 'q')
+            {
+                a = char.Parse(Console.ReadLine());
+                Console.WriteLine(a + " -> " + char.ToUpper(a));
+            }
+        }
+
+        static void StrF13()
+        {
+            string str = "В лесу родилась елочка в лесу она росла зимой и летом стройная зеленая была";
+            string[] arr = str.Split(' ');          
+
+            for (int i=0;i<arr.Length;i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            Console.ReadLine();
+        }
+        static void StrF12()
+        {
+            string num = "slsnlsj 131321 23123";
+            string[] temp = num.Split(' ');
+            int chislo = 0;
+            int sum = 0;
+
+            foreach (string i in temp )
+            {
+                if (int.TryParse(i, out chislo))
+                {
+                    sum += chislo;
+                }
+            }
+            Console.WriteLine("Summa:"+sum);
+            Console.ReadLine();
+        }
+        static void StrF11()
+        {
+            string a = "hsadas 1 4 asd 6 a 3";
+            string[] temp = a.Split(' ');
+
+
+            int count = 0;
+            int chislo = 0;
+            foreach (string i in temp)
+            {
+                if (int.TryParse(i, out chislo))
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ConsoleApp1.Practice3/ConsoleApp1.Practice3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ConsoleApp1.Practice3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp1.Practice3")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("f9f3f368-117a-4bd8-996c-26da189c8af7")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
