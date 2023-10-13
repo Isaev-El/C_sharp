9a29b9a509c1f421c287aecaf59eb456b1901964
Isaev-El <isaev.elkhan2003@gmail.com>
2023-10-13 23:19:59 +0600

    pract 5

---
+++ ConsoleApp1.Practice5/ConsoleApp1.Practice5/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Practice5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                CallMethodThatThrowsException();
+            }
+
+            catch (CustomException ex)
+            {
+                Console.WriteLine("Исключение перехвачено в методе Main:");
+                Console.WriteLine(ex.Message);
+            }
+        }
+        static void CallMethodThatThrowsException()
+        {
+            Console.WriteLine("Вызывающий метод: начало выполнения");
+
+            try
+            {
+                // Вызываем метод, который сгенерирует исключение
+                ThrowCustomException();
+            }
+            catch (CustomException ex)
+            {
+                Console.WriteLine("Исключение перехвачено в вызывающем методе:");
+                Console.WriteLine(ex.Message);
+                throw; // Поднимаем исключение выше
+            }
+
+            Console.WriteLine("Вызывающий метод: конец выполнения");
+        }
+
+        static void ThrowCustomException()
+        {
+            Console.WriteLine("Метод, который генерирует исключение: начало выполнения");
+            throw new CustomException("Произошла пользовательская ошибка");
+        }
+    }
+    public class CustomException : Exception
+    {
+        public CustomException(string message) : base(message)
+        {
+
+        }
+    }
+}
